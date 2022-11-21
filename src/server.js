4ef3136f50e2522d01c3371871ecab0ef3877385
Oleg Polyanskiy <ol.polyanskiy@gmail.com>
2022-11-21 23:40:19 +0300

    feat: modify server code

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.resolve(__dirname, '../dist')));
+app.get('/*', (req, res) => res.sendFile(path.resolve(__dirname, '../dist/index.html')));
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
